c4a056836e8534c52ffda5bfd60bade4c231bd83
catmanscodes <vimal.maru10083@gmail.com>
2020-11-12 12:21:33 +0530

    Add bean scope demo

---
+++ 03_SpringCore/Spring-Ioc-Xml-Config-Bean-Scopes/src/org/catmanscodes/main/service/AnimalServiceImpl.java
@@ -0,0 +1,10 @@
+package org.catmanscodes.main.service;
+
+public class AnimalServiceImpl implements AnimalService {
+
+	@Override
+	public void getAnimal() {
+		System.out.println("Hello Spring Bean scopes example");
+	}
+
+}
